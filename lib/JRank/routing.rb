668de5becf02ef02fd51727db2504941faae8b70
Paul Ketelle <paul@ketelle.com>
2011-08-15 17:36:13 +0100

    adding mapping

--- lib/JRank/routing.rb
+++ lib/JRank/routing.rb
@@ -1,14 +1,3 @@
-require 'xml-object'
-require 'open-uri'
-module Jrank
-  module Routing
-	module MapperExtensions
-      def jrank
-        @set.add_route("/search", {:controller => "jrank_controller", :action => "index"})
-        @set.add_route("/search?q=:query(&start=:start)(&limit=:limit)", {:controller => "jrank_controller", :action => "show"})
-      end
-    end
-  end
+Rails.application.routes.draw do |map|
+	map.resources :search, :only => [ :index, :show ]
 end
-
-ActionController::Routing::RouteSet::Mapper.send :include, Jrank::Routing::MapperExtensions
