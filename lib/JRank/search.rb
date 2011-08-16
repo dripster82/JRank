919ba348a3508e1c9d7a41652b00ac2a75bb685b
Paul Ketelle <paul@ketelle.com>
2011-08-16 16:12:06 +0100

    adding mapping

--- lib/JRank/search.rb
+++ lib/JRank/search.rb
@@ -8,7 +8,7 @@ module Jrank
 	end
 	
 	def find(query, start = 0, limit = 10)
-		XMLObject.new(open("http://www.jrank.org/api/search/v2.xml?key=420261e39acb932299ec5098e009b4828f9cefa3&q=#{query}&start=#{start}&limit=#{limit}"))
+		XMLObject.new(open("http://www.jrank.org/api/search/v2.xml?key=420261e39acb932299ec5098e009b4828f9cefa3&q=#{URI.escape(query)}&start=#{URI.escape(start)}&limit=#{URI.escape(limit)}"))
 	end
 	
   end
